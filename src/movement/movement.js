6c0648e0e7681f2d4be81b2f3b9e793fb07c8906
Spencer Faust <faus0059@umn.edu>
2019-05-08 12:47:00 -0500

    Playing around with images.

--- src/movement/movement.js
+++ src/movement/movement.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './movement.css';
+import './spin.png';
 
 class Movement extends Component {
 
     render() {
         return (
             <div className="placement">
-                < img alt="Spinny" href = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfQ8tsy3Y3B3TCQbqtw99idZ_BQ3uBofpxVZVdCCnTSx9t15SB" />
+                < img alt = "Spinny"
+                src = "https://www.risingstars.com.ua/pictures/other-acts/stunt-acts/power-tumblers-gymnasts-acrobats/special-tumbler-acrobat-617/2.jpg" / >
             </div>
         )
     }

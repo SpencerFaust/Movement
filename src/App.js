0225fd48c5d87da69ab9f9fe294c6061c75b6db0
Spencer Faust <faus0059@umn.edu>
2019-05-08 12:40:54 -0500

    Working on sizing ideas.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Movement from './movement';
+import Movement from './movement/movement';
 
 function App() {
   return (
---
+++ src/movement/movement.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './movement.css';
+
+class Movement extends Component {
+
+    render() {
+        return (
+            <div className="placement">
+                < img alt="Spinny" href = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfQ8tsy3Y3B3TCQbqtw99idZ_BQ3uBofpxVZVdCCnTSx9t15SB" />
+            </div>
+        )
+    }
+}
+
+export default Movement;
